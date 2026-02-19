30d2a0117003b1a4191624e529e259d0b8336eca
Oleksii Gapchenko <deusson@gmail.com>
2026-02-19 20:59:36 +0100

    fix(GET-132): patch CommonJS React act shim

--- the-getaway/src/setupTests.ts
+++ the-getaway/src/setupTests.ts
@@ -2,7 +2,16 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === 'production') {
   process.env.NODE_ENV = 'test';
 }
 
+import { createRequire } from 'module';
+import { act as reactAct } from 'react';
 import '@testing-library/jest-dom';
+
+const nodeRequire = createRequire(__filename);
+const reactCjs = nodeRequire('react');
+
+if (!reactCjs.act) {
+  reactCjs.act = reactAct;
+}
 
 // Polyfill scrollIntoView for jsdom environment
 Object.defineProperty(HTMLElement.prototype, 'scrollIntoView', {
